d6510ca6292d176e5a1309eba985af358cdd8461
DESKTOP-OGLLSNF\AGL <nguyen_xuan_hiep@allgrow-labo.jp>
2022-05-05 17:25:41 +0700

    first commit

---
+++ src/static/assets/scripts/app.js
@@ -0,0 +1,20 @@
+let $ = document.querySelector.bind(document);
+let $$ = document.querySelectorAll.bind(document);
+let header = $(".c-header");
+
+window.onscroll = function () {
+  handleScrollTopHeader();
+};
+
+function handleScrollTopHeader() {
+  let scrollTopHeader =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0;
+  if (scrollTopHeader > 50) {
+    header.classList.add("is-active");
+  } else {
+    header.classList.remove("is-active");
+  }
+}
