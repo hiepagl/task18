a3e86f287518c74910a58b2f6112875d7caf7f22
DESKTOP-OGLLSNF\AGL <nguyen_xuan_hiep@allgrow-labo.jp>
2022-05-06 17:17:58 +0700

    2nd commit

--- src/static/assets/scripts/app.js
+++ src/static/assets/scripts/app.js
@@ -1,6 +1,15 @@
 let $ = document.querySelector.bind(document);
 let $$ = document.querySelectorAll.bind(document);
+// header
 let header = $(".c-header");
+let headerNavList = $(".c-header__navList");
+let showNavSub1 = $("#show-navSub1");
+let showNavSub2 = $("#show-navSub2");
+let goBack = $("#goBack");
+
+// slider
+let slider = $(".p-slider__wrapper");
+let sliderControl = $$(".p-slider__controlItem");
 
 window.onscroll = function () {
   handleScrollTopHeader();
@@ -17,4 +26,37 @@ function handleScrollTopHeader() {
   } else {
     header.classList.remove("is-active");
   }
+}
+
+goBack.onclick = function () {
+  headerNavList.classList.remove("is-navSub1", "is-navSub2");
+};
+
+showNavSub1.onclick = function () {
+  headerNavList.classList.add("is-navSub1");
+};
+
+showNavSub2.onclick = function () {
+  headerNavList.classList.add("is-navSub2");
+};
+
+let clearSlider = function () {
+  for (let i = 0; i < sliderControl.length; i++) {
+    slider.classList.remove(`p-slider__visual${i + 1}`);
+  }
+};
+
+let clearSliderControl = function () {
+  for (let i = 0; i < sliderControl.length; i++) {
+    sliderControl[i].classList.remove("is-active");
+  }
+};
+
+for (let i = 0; i < sliderControl.length; i++) {
+  sliderControl[i].onclick = function () {
+    clearSlider();
+    clearSliderControl();
+    slider.classList.add(`p-slider__visual${i + 1}`);
+    sliderControl[i].classList.add("is-active");
+  };
 }
